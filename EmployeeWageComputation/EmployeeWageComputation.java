314f9323822b042f1f000acf62d8cfd581b04da7
Birajit <birajit95@gmail.com>
2020-10-28 22:47:55 +0530

    Add: Condition based wage is computed for a month

--- EmployeeWageComputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/EmployeeWageComputation.java
@@ -5,9 +5,9 @@ public class EmployeeWageComputation{
     private final int FULL_TIME=8;
     private final int PART_TIME=4;
     private final int MAX_NO_OF_DAYS=20;
-
-
-
+    private final int MAX_WORKING_HOURS=160;
+    private static final int MAX_DAYS_IN_MONTH=30;
+    
     public static void welcomeMessage(){
         System.out.println("Welcome to Employee Wage Computation Program");
     }
@@ -39,21 +39,39 @@ public class EmployeeWageComputation{
            return workingHourPerDay;
     }
 
-    public int getDailyWage(){
-        int workingHourPerDay=this.getWorkingHourPerDay();
+    public int getDailyWage(int perDayWorkHour){
+        int workingHourPerDay=perDayWorkHour;
         return workingHourPerDay*this.WAGE_PER_HOUR;
     }
 
     public int getMonthlyWage(){
-        return this.getDailyWage()*this.MAX_NO_OF_DAYS;
+        return this.getDailyWage(this.getWorkingHourPerDay())*this.MAX_NO_OF_DAYS;
     }
-
+    
     public static void main(String[] args){
         EmployeeWageComputation.welcomeMessage();
         
         EmployeeWageComputation empObj = new EmployeeWageComputation();
-        System.out.println(empObj.getAttendance());
-        System.out.println(empObj.getDailyWage());
+        
+        int totalWorkingHour=0;
+        int totalWorkingDays=0;
+        int totalDays=0;
+        int totalWage=0;
+        int perDayWorkHour=0;
+        int maxDaysInMonth=EmployeeWageComputation.MAX_DAYS_IN_MONTH;
 
+        while(totalWorkingHour<empObj.MAX_WORKING_HOURS && totalWorkingDays<empObj.MAX_NO_OF_DAYS && totalDays<maxDaysInMonth){
+            totalDays++;
+            if(empObj.getAttendance().equals("present")){
+                totalWorkingDays++;
+                perDayWorkHour=empObj.getWorkingHourPerDay();
+                totalWorkingHour=totalWorkingHour+perDayWorkHour;
+                totalWage=totalWage+empObj.getDailyWage(perDayWorkHour);
+                
+                System.out.println("Day-"+totalDays+"=> working hours : "+perDayWorkHour);
+            }
+
+        }
+        System.out.println("Total Wage: "+totalWage); 
     }
 }
