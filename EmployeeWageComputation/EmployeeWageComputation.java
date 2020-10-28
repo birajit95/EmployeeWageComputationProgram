a5c32d9881815744ff9a6d7f0f963c8ff92b5f57
Birajit <birajit95@gmail.com>
2020-10-28 17:47:43 +0530

    Add: Welcome message is displayed

---
+++ EmployeeWageComputation/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+public class EmployeeWageComputation{
+
+    public static void welcomeMessage(){
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+    public static void main(String[] args){
+        EmployeeWageComputation.welcomeMessage();
+        
+    }
+}
