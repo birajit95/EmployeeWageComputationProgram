0009e7a3e040b1db80ec8ed93f70d5e7994a9071
Birajit <birajit95@gmail.com>
2020-10-28 17:58:04 +0530

    Add: Employee attendance is checked

--- EmployeeWageComputation/EmployeeWageComputation.java
+++ EmployeeWageComputation/EmployeeWageComputation.java
@@ -3,8 +3,20 @@ public class EmployeeWageComputation{
     public static void welcomeMessage(){
         System.out.println("Welcome to Employee Wage Computation Program");
     }
+
+    public String getAttendance(){
+        if(Math.random()*10>=3){
+            return "present";
+        }
+        else{
+            return "absent";
+        }
+    }
+
     public static void main(String[] args){
         EmployeeWageComputation.welcomeMessage();
         
+        EmployeeWageComputation empObj = new EmployeeWageComputation();
+        System.out.println(empObj.getAttendance());
     }
 }
